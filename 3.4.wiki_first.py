c4b082cf34202f79e77ee02c14f99156c6b4782f
HardPlant <abc7988se@naver.com>
2018-02-20 17:08:15 +0900

    random wiki machine

--- 3.4.wiki_first.py
+++ 3.4.wiki_first.py
@@ -1,5 +1,7 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import datetime
+import random
 import re
 
 '''
@@ -8,22 +10,43 @@ import re
 
 pages = set()
 
+def get_random_links(links):
+    while True:
+        link = links[random.randint(0, len(links) -1)]
+        if 'href' in link.attrs:
+            if link.attrs['href'] not in pages:
+                return link
+
 def getLinks(link):
     global pages
     html = urlopen('https://en.wikipedia.org'+link) # link는 /wiki/~ 형태로 반환된다.
     bsObj = BeautifulSoup(html, "html.parser")
+    try:
+        print(bsObj.h1.get_text())
+        print(bsObj.find("div",{'id':'mw-content-text'}).findAll("p")[0].get_text())
+        print(bsObj.find("li", {'id':'ca-edit'}).find("span").find("a").attrs['href'])
+    except AttributeError:
+        print("This page is missing something.")
 
-    print(bsObj.h1.get_text())
-    print(bsObj.find("div",{'id':'mw-content-text'}).findAll("p")[0])
-    print(bsObj.find("li", {'id':'ca-edit'}).find("span").find("a"))
+    link = get_random_links(bsObj.find("div",{'id':'bodyContent'}).findAll("a", href=re.compile('^(/wiki/)((?!:).)*$')))
+    newPage = link.attrs['href']
+    print("--------")
+    print(newPage)
+    pages.add(newPage)
+    getLinks(newPage)
 
+    '''
     for link in bsObj.find("div",{'id':'bodyContent'}).findAll("a", href=re.compile('^(/wiki/)((?!:).)*$')):
         if 'href' in link.attrs:
             if link.attrs['href'] not in pages:
                 newPage = link.attrs['href']
+                print("--------")
                 print(newPage)
                 pages.add(newPage)
                 getLinks(newPage)
-    
+    '''
+
+
+
 #url = '/wiki/Kevin_Bacon'
 links = getLinks('')
