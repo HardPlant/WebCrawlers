b2495be2afe4e6e044141611fb11081ce11501a6
HardPlant <abc7988se@naver.com>
2018-02-20 19:10:34 +0900

    update

--- 4.1.twitter.py
+++ 4.1.twitter.py
@@ -1,6 +1,5 @@
 from twitter import *
 
-
 with open('apikey') as file:
     key = file.read()
     keys = key.split(',')
@@ -8,13 +7,22 @@ with open('apikey') as file:
     access_token_secret = keys[1].strip()
     consumer_key = keys[2].strip()
     consumer_secret = keys[3].strip()
-print(access_token)
-print(access_token_secret)
-print(consumer_key)
-print(consumer_secret)
 
 t = Twitter(auth=OAuth(access_token, access_token_secret,\
             consumer_key, consumer_secret))
 
+'''트윗을 가져온다.
 pythonTweets = t.search.tweets(q="#python")
 print(pythonTweets)
+'''
+
+'''상태를 업데이트한다.
+statusUpdate = t.statuses.update(status='Hello,World!')
+print(statusUpdate)
+'''
+
+
+'''목록을 가져온다
+pythonStatuses = t.statuses.user_timeline(screen_name="montypython", count=5)
+print(pythonStatuses)
+'''
