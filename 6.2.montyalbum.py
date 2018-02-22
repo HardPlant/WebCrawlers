756574dff68d60ba13cc0101ec99fb108b1a82eb
HardPlant <abc7988se@naver.com>
2018-02-22 19:08:40 +0900

    dictreader

---
+++ 6.2.montyalbum.py
@@ -0,0 +1,14 @@
+from urllib.request import urlopen
+from io import StringIO
+import csv
+
+data = urlopen("http://pythonscraping.com/files/MontyPythonAlbums.csv")\
+    .read().decode('ascii', 'ignore')
+dataFile = StringIO(data)
+#csvReader = csv.reader(dataFile)
+dictReader = csv.DictReader(dataFile) # csvReader는 첫 번째 행(표의 헤더)를 무시하지 않는다.
+
+print(dictReader.fieldnames)
+
+for row in dictReader:
+    print(row)
