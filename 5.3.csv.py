fa1dc898183027e5c9d9a06e0330135f90fe7ffd
HardPlant <abc7988se@naver.com>
2018-02-22 20:47:32 +0900

    level 2

--- 5.3.csv.py
+++ 5.3.csv.py
@@ -1,10 +1,18 @@
 import csv
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
-csvFile = open("../files/test.csv", 'w+')
+html = urlopen("https://en.wikipedia.org/wiki/Comparison_of_text_editors")
+bsObj = BeautifulSoup(html, 'html.parser')
+table = bsObj.findAll('table', {'class':'wikitable'})[0]
+rows = table.findAll("tr")
+csvFile = open("editors.csv", 'wt')
+writer = csv.writer(csvFile)
 try:
-    writer = csv.writer(csvFile)
-    writer.writerow(('number', 'number plus 2', 'number times 2'))
-    for i in range(10):
-        writer.writerow( (i, i+2, i*2))
+    for row in rows:
+        csvRow = []
+        for cell in row.findAll(['td', 'th']):
+            csvRow.append(cell.get_text())
+        writer.writerow(csvRow)
 finally:
     csvFile.close()
