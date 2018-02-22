dd5cf732e9c0e7e5f33506d57b70955fb803ac88
HardPlant <abc7988se@naver.com>
2018-02-22 20:01:34 +0900

    level3

--- 7.1.n-gram.py
+++ 7.1.n-gram.py
@@ -1,18 +1,31 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
+import string
 
-def ngrams(input, n):
+def clean_input(input):
     input = re.sub('\n+', " ", input)
+    input = re.sub('\[[0-9]*\]', "", input)
     input = re.sub(' +', " ", input)
     input = bytes(input, 'UTF-8')
     input = input.decode("ascii", "ignore")
-    print(input)
+    clean_input = []
     input = input.split(' ')
+    for item in input:
+        item = item.strip(string.punctuation)
+        if len(item) > 1 or (item.lower() == 'a' or item.lower() == 'i'):
+            clean_input.append(item)
+
+    return clean_input
+
+def ngrams(input, n):
+    input = clean_input(input)
     output = []
     for i in range(len(input)-n+1):
         output.append(input[i:i+n])
+
     return output
+    
 
 html = urlopen('http://en.wikipedia.org/wiki/Python_(programming_language)')
 bsObj = BeautifulSoup(html, "html.parser")
