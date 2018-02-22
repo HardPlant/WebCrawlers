a5d2c119a5453185ba243b0070dfbc6af972d746
HardPlant <abc7988se@naver.com>
2018-02-23 00:49:26 +0900

    example

--- 10.1.ajax.py
+++ 10.1.ajax.py
@@ -6,3 +6,16 @@ driver.get('http://pythonscraping.com/pages/javascript/ajaxDemo.html')
 time.sleep(3)
 print(driver.find_element_by_id('content').text)
 driver.close()
+
+'''
+driver.find_element_by_css_selector("#content")
+driver.find_element_by_name("div")
+
+driver.find_elements_by_css_selector("#content")
+driver.find_elements_by_name("div")
+
+pageSource = driver.page_source
+bsObj = BeautifulSoup(pageSource)
+print(bsObj.find(id="content").get_text())
+'''
+
