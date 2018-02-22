7fed44611527e1bc7c7a50a7ab6043eb58a5b0c5
HardPlant <abc7988se@naver.com>
2018-02-22 23:52:38 +0900

    level 2 : session

--- 9.2.request_cookie.py
+++ 9.2.request_cookie.py
@@ -1,7 +1,9 @@
 import requests
+session = requests.Session()
+
 params = {'username':'Ryan', 'password':'password'}
-r=requests.post("http://pythonscraping.com/pages/cookies/welcome.php", params)
+r=session.post("http://pythonscraping.com/pages/cookies/welcome.php", params)
 print("Cookie is set to:")
 print(r.cookies.get_dict())
-r = requests.get("http://pythonscraping.com/pages/cookies/profile.php", cookies=r.cookies)
+r = session.get("http://pythonscraping.com/pages/cookies/profile.php", cookies=r.cookies)
 print(r.text)
