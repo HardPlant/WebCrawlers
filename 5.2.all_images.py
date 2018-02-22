890a7a96d3563707ba536813f21390a464461041
HardPlant <abc7988se@naver.com>
2018-02-22 16:48:40 +0900

    it works.

--- 5.2.all_images.py
+++ 5.2.all_images.py
@@ -10,7 +10,7 @@ baseUrl = "http://pythonscraping.com"
 def getAbsoluteURL(baseUrl, source):
     if source.startswith("http://www."):
         url = "http://"+source[11:]
-    elif source.startswith("http://")
+    elif source.startswith("http://"):
         url = source
     elif source.startswith("www."):
         url = "http://"+source[4]
@@ -28,8 +28,18 @@ def getDownloadPath(baseUrl, absoluteUrl, downloadDirectory):
     
     directory = os.path.dirname(path)
 
-    if not os.path.exist(directory):
+    if not os.path.exists(directory):
         os.makedirs(directory)
     
     return path
 
+html = urlopen("http://www.pythonscraping.com")
+bsObj = BeautifulSoup(html, "html.parser")
+downloadList = bsObj.findAll(src=True)
+
+for download in downloadList:
+    fileUrl = getAbsoluteURL(baseUrl,download["src"])
+    if fileUrl is not None:
+        print(fileUrl)
+
+urlretrieve(fileUrl, getDownloadPath(baseUrl, fileUrl, downloadDirectory))
