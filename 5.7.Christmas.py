bb4271a2cc2e49d13f7998fd154d652736d38509
HardPlant <abc7988se@naver.com>
2018-02-22 18:19:10 +0900

    christmas_checker

---
+++ 5.7.Christmas.py
@@ -0,0 +1,37 @@
+import smtplib
+from email.mime.text import MIMEText
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import time
+
+with open('mail') as file:
+    key = file.read()
+    keys = key.split(',')
+    username = keys[0].strip()+'@gmail.com'
+    password = keys[1].strip()
+
+def send_mail(subject, body):
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = username
+    msg['To'] = "abc7988jpse@gmail.com"
+
+    try:
+        s = smtplib.SMTP('smtp.gmail.com:587')
+        s.ehlo()
+        s.starttls()
+        s.login(username, password)
+        s.send_message(msg)
+        s.quit()
+        print("Successed!")
+    except Exception as e:
+        print("Failed", e)
+
+bsObj = BeautifulSoup(urlopen("https://isitchristmas.com/"))
+while(bsObj.find("a", {"id":"answer"}).attrs['title'] == "NO"):
+    print("It is not Christmas yet.")
+    time.sleep(3000)
+
+bsObj = BeautifulSoup(urlopen("https://isitchristmas.com/"))
+send_mail("It's Christmas!"
+,"According to http://itischristmas.com, it is Christmas!")
