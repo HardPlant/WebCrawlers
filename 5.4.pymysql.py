b8ec7079a964de4dc5b80da4b4b263792fc69956
HardPlant <abc7988se@naver.com>
2018-02-22 17:13:18 +0900

    mysql integrated

--- 5.4.pymysql.py
+++ 5.4.pymysql.py
@@ -5,3 +5,14 @@ import datetime
 import random
 import pymysql
 
+with open('mysql') as file:
+    key = file.read()
+    keys = key.split(',')
+    ip = keys[0].strip()
+    user = keys[1].strip()
+    password = keys[2].strip()
+
+conn = pymysql.connect(host=ip, user=user,passwd=password, db='mysql', charset='utf8')
+
+cur = conn.cursor()
+cur.execute("USE scraping")
